77be8917d33be789449ab6dcae61df436076041b
priz789 <40210603+priz789@users.noreply.github.com>
2019-02-06 16:26:50 +0200

    06.02
    Added main menu class. Realized choosing method.

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,26 @@
-#include<iostream>
 #include "Field.h"
+#include "Menu.h"
+#include "Functions.h"
 
 using namespace std;
+
+void ClearBuffer();
 
 int main()
 {
 	setlocale(LC_ALL, "Russian"); //Подключение русских символов
-	Field F1;
+	Menu MainMenu;
+	MainMenu.RunMenu();
+	cout << MainMenu.getChoice() << endl;
+	/*Field F1;
 	Field F2;
 	F1.Draw();
 	cout << endl;
-	F2.Draw();
+	F2.Draw();*/
+}
+
+void ClearBuffer()
+{
+	cin.clear();
+	cin.ignore(cin.rdbuf()->in_avail());
 }
