4bf9373f5270b6f9067985f7eb411c3d2d86c66f
priz789 <40210603+priz789@users.noreply.github.com>
2019-02-12 17:02:27 +0200

    12.02_1

--- Menu.cpp
+++ Menu.cpp
@@ -16,6 +16,12 @@ void Menu::AddOption(string _OptionName)
 	vecStrOptions.insert(it, _OptionName);
 	numberOfOptions = vecStrOptions.size();
 }
+void Menu::AddMenu(const SubMenu* _SubMenu)
+{
+	vector<SubMenu*>::iterator it;
+	it = vecSubMenues.begin();
+	vecSubMenues.insert(it, _SubMenu);
+}
 void Menu::ShowMenu()
 {
 	system("cls");
@@ -31,5 +37,36 @@ int Menu::GetChoice()
 	{
 		choice = _getch();
 	} while ((choice < 49 && choice > (49+numberOfOptions))||choice == 27);
+	return choice;
+}
+
+SubMenu::SubMenu()
+{
+	choice = 0;
+	vecStrOptions.push_back("Exit");
+	numberOfOptions = vecStrOptions.size();
+}
+void SubMenu::AddOption(string _OptionName)
+{
+	vector<string>::iterator it;
+	it = vecStrOptions.begin();
+	vecStrOptions.insert(it, _OptionName);
+	numberOfOptions = vecStrOptions.size();
+}
+void SubMenu::ShowSubMenu()
+{
+	system("cls");
+	for (int i = 0; i < numberOfOptions; i++)
+	{
+		cout << "\t" << i + 1 << ". " << vecStrOptions[i] << ".\n";
+	}
+}
+
+int SubMenu::GetChoice()
+{
+	do
+	{
+		choice = _getch();
+	} while ((choice < 49 && choice >(49 + numberOfOptions)) || choice == 27);
 	return choice;
 }
--- Menu.h
+++ Menu.h
@@ -11,9 +11,23 @@ private:
 	int numberOfOptions;
 	int choice;
 	vector<string>vecStrOptions;
+	vector<SubMenu*>vecSubMenues;
 public:
 	Menu();
 	void AddOption(string _OptionName);
+	void AddMenu(const SubMenu* _SubMenu);
 	void ShowMenu();
+	int GetChoice();
+};
+
+class SubMenu {
+private:
+	int numberOfOptions;
+	int choice;
+	vector<string>vecStrOptions;
+public:
+	SubMenu();
+	void AddOption(string _OptionName);
+	void ShowSubMenu();
 	int GetChoice();
 };
